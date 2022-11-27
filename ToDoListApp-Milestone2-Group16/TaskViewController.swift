3c10e99d331f38f6c88066661582205515097ac5
Wilson Mungai <wilson@Wilsons-MacBook-Air.local>
2022-11-27 17:47:55 -0500

    added UI elements

---
+++ ToDoListApp-Milestone2-Group16/TaskViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskViewController.swift
+//  ToDoListApp-Milestone2-Group16
+//
+//  Created by Wilson Mungai on 2022-11-27.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
